dd5bd782c69d8c3f869ee16062c6d6701c97a5fc
Smah Riki <86798574+sr1k1@users.noreply.github.com>
2026-04-21 16:48:38 -0700

    added json body parsing and static file serving, and implemented request Id attribute to request, method logging, and error handling

--- week-3-middleware/app.js
+++ week-3-middleware/app.js
@@ -1,13 +1,67 @@
-const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
-const dogsRouter = require('./routes/dogs');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const dogsRouter = require("./routes/dogs");
+
+// Import status codes
+const { StatusCodes } = require("http-status-codes");
 
 const app = express();
 
-// Your middleware here
+// =============== Your middleware here =============== //
 
-app.use('/', dogsRouter); // Do not remove this line
+// == Middleware for all requests == //
+// Create and add unique requestID to all requests
+app.use((req, res, next) => {
+  req.requestId = uuidv4();
 
-const server =	app.listen(3000, () => console.log("Server listening on port 3000"));
+  // Set requestID to response header
+  res.setHeader("X-Request-Id", req.requestId);
+  next();
+});
+
+// Log timestamp
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+
+  console.log(`[${timestamp}]: ${req.method} ${req.path} (${req.requestId})`);
+  next();
+});
+
+// security headers
+
+// body parsing
+app.use(express.json());
+
+// content-type validation
+
+// Validate POST request header
+// app.use((req, res, next) => {
+//   if (req.method === "POST") {
+//     if (req.headers["content-type"] !== "application/json") {
+//       res.status(StatusCodes.BAD_REQUEST).json({message: "Incorrect content type."});
+//     }
+//     return;
+//   }
+//   next();
+// });
+
+// static image serving
+app.use(express.static(path.join(__dirname, "public")));
+
+//routes
+app.use("/", dogsRouter); // Do not remove this line
+
+// error handling
+app.use((err, req, res, next) => {
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ error: "Internal Server Error", requestId: req.requestId });
+});
+
+// 404 handler
+
+const server = app.listen(3000, () =>
+  console.log("Server listening on port 3000"),
+);
 module.exports = server;
--- week-3-middleware/routes/dogs.js
+++ week-3-middleware/routes/dogs.js
@@ -3,22 +3,22 @@ const router = express.Router();
 const dogs = require("../dogData.js");
 
 router.get("/dogs", (req, res) => {
-	res.json(dogs);
+  res.json(dogs);
 });
 
 router.post("/adopt", (req, res) => {
-    const { name, address, email, dogName } = req.body;
-    if (!name || !email || !dogName) {
-        return res.status(400).json({ error: "All fields are required" });
-    }
+  const { name, address, email, dogName } = req.body;
+  if (!name || !email || !dogName) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
 
-    return res.status(201).json({
-        message: `Adoption request received. We will contact you at ${email} for further details.`,
-    });
+  return res.status(201).json({
+    message: `Adoption request received. We will contact you at ${email} for further details.`,
+  });
 });
 
 router.get("/error", (req, res) => {
-	throw new Error("Test error");
+  throw new Error("Test error");
 });
 
 module.exports = router;
