459b619d26832008ce44d9ab650e7ae7a4c482a7
Smah Riki <86798574+sr1k1@users.noreply.github.com>
2026-06-15 19:24:58 -0700

    added analyticsMiddleware for use in analyticsController

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -8,11 +8,17 @@ const {
 // import jwtMiddleware
 const jwtMiddleware = require("./../middleware/jwtMiddleware");
 
+// import analytics middleware
+const analyticsMiddleware = require("./../middleware/analyticsMiddleware");
+
 // Create router to hold all analytics routes
 const router = express.Router();
 
 // Ensure logged-in user using jwtMiddleware
 router.use(jwtMiddleware);
+
+// Ensure that only managers are accessing this route
+router.use(analyticsMiddleware);
 
 // User Routes
 router.route("/users/:id").get(getUserAnalytics);
